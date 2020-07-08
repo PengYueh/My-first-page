10519e9bb9b8987b4028b9fba964bdd4417f133c
Yueh Peng <joyce840613@gmail.com$ git config --global user.email joyce840613@gmail.com>
2020-07-08 23:52:33 +0800

    0708

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,17 +1,43 @@
 $(document).ready(function () {
-    $('.login').on('click',function () {
+    $('#login').on('click',function () {
         $('.login-wrapper').fadeIn()
     })
     $('.dark-mask').on('click',function () {
         $('.login-wrapper').fadeOut()
     })
     $('.btn-login').on('click',function () {
-        $('.login-homepage').fadeOut(),
-        $('.login-page').fadeIn()
+        $('.login-homepage').toggle(),
+        $('.login-page').toggle()
     })
     $('.last-page').on('click',function () {
-        $('.login-page').fadeOut(),
-        $('.login-homepage').fadeIn()
+        $('.login-page').toggle(),
+        $('.login-homepage').toggle()
     })
+
+
+    $('#logout').on('click',function(){
+        firebase.auth().signOut().then(function(){
+            window.location.reload()
+        })
+    })
+
+    $('#logout').hide()
+    $('#login').hide()
+
+    firebase.auth().onAuthStateChanged(
+        function(user){
+           if(user){
+                $('#logout').show()
+                $('#login').hide()
+                            
+            }else{
+                $('#login').show()
+                $('#logout').hide()
+            } 
+        }        
+    )
+
+
+
 });
 
---
+++ assets/js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBoA5X1LqPjauCU16ZJVkftOZu1zlA4iOg",
+    authDomain: "my-first-page-b5e93.firebaseapp.com",
+    databaseURL: "https://my-first-page-b5e93.firebaseio.com",
+    projectId: "my-first-page-b5e93",
+    storageBucket: "my-first-page-b5e93.appspot.com",
+    messagingSenderId: "503399319566",
+    appId: "1:503399319566:web:f8d932f67e5ef41dbd545f"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
